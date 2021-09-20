7691c3709410ad5a327d946f57e0ce729f9b4c01
Rahul Wavare <rahulrakida@gmail.com>
2021-09-20 11:26:55 +0300

    don't need this stuff
    lol

--- begentle/__init__.py
+++ begentle/__init__.py
@@ -44,34 +44,3 @@ class CommentAnalyzer():
         response = self.client.comments().analyze(body=request).execute()
         toxicity = response['attributeScores']['TOXICITY']['summaryScore']['value']
         return toxicity
-"""
-client = discovery.build(
-    "commentanalyzer",
-    "v1alpha1",
-    developerKey=API_KEY,
-    discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
-    static_discovery=False
-)
-
-def comment_toxicity(message: str):
-    
-    # Returns a tuple containing if the comment
-    # is toxic, and a value from `0` to `1` representing
-    # toxicity. Toxicity is defined by a value
-    # above `0.9`.
-    # Sample: `(True, 0.97173126)`
-    
-    request = {
-        'comment': { 'text': message },
-        'requestedAttributes': {'TOXICITY': {}}
-    }
-
-    response = client.comments().analyze(body=request).execute()
-    toxicity = response['attributeScores']['TOXICITY']['summaryScore']['value']
-    if toxicity >= 0.9:
-        is_toxic = True
-    else:
-        is_toxic = False
-
-    return (is_toxic, toxicity)
-"""
